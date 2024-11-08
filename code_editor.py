dcfe45c0b13599cdc9b93b672b7126428debdb0d
Aliya Gamez <aliya.e.gamez@gmail.com>
2024-11-08 11:26:40 -0500

    creating new code editor initial files

--- code_editor.py
+++ code_editor.py
@@ -1,3 +1,5 @@
+# code_editor.py (MAIN)
+
 import os
 import re
 import tkinter as tk
@@ -6,17 +8,32 @@ from tkinter.filedialog import asksaveasfilename, askopenfilename
 from editor_styling import *
 from syntax_patterns import syntax_patterns
 
-class CodeEditor(self, *args, **kwargs):
-    def __init__(self, *args, **kwargs):
+class EditorStyling:
+    def __init__(self,*args,**kwargs):
+        self.
+
+class CodeEditor(tk.Frame):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.editor = tk.Text(self)
+        self.vsb = tk.Scrollbar(self,orient=tk.VERTICAL,command=self.editor.yview)
+
+        self.editor.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side=tk.RIGHT,fill=tk.Y,expand=1)
+        self.editor.pack(side=tk.RIGHT,fill=tk.BOTH)
+
 
 class MainApp:
     def __init__(self):
+        # Create main window to place UI modules in
         self.root = tk.Tk()
-        self.code_editor = CodeEdtior(self.root)
-        self.code_editor.pack(side="top",fill="tk.BOTH",expand=True)
+        self.code_editor = CodeEditor(self.root)
+        self.code_editor.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
     def program_run(self):
         self.root.mainloop()
 
 if __name__=="__main__":
-    root = tk.Tk()
+    app = MainApp()
+    app.program_run()
 
