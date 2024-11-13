7ce67ae4bded5cb8c916a1379939d25bc111ffa1
Aliya Gamez <aliya.e.gamez@gmail.com>
2024-11-13 15:43:52 -0500

    added unused code, code that might still make it

---
+++ docs/unused-code.py
@@ -0,0 +1,21 @@
+# pochacode/__main__.py
+
+# pochacode/code_editor.py
+
+    self.action_bar.grid(row=0, column=0, columnspan=3, sticky="nsew") # Action Bar (not created yet but placed for future reference)
+    # (0,0) Action Bar (Span 3 Columns) (not created yet but placed for future reference)
+    self.grid_rowconfigure(0,weight=0)
+    self.grid_columnconfigure(0,weight=1)
+
+    # from def_select_all
+    self._editor.see(tk.INSERT) # move to cursor
+
+# pochacode/menu_bar.py
+
+# pochacode/editor_styling.py
+
+# pochacode/syntax_highlight.py
+
+# pochacode/utils/text_line_numbers.py
+
+# pochacode/utils/synced_scrollbar.py
